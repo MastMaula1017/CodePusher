ace3e227420b6059d7c10b0ba866b3e8181e8a83
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-07 18:25:45 +0530

    CodePusher: Solution for 2. Add Two Numbers

---
+++ Linked List/2. Add Two Numbers/2. Add Two Numbers.cpp
@@ -0,0 +1,32 @@
+// Problem: 2. Add Two Numbers
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 77.2 MB (Beats 13.20%)
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0);
+        ListNode* curr = &dummy;
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry) {
+            int sum = carry;
+
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            carry = sum / 10;
+            curr->next = new ListNode(sum % 10);
+            curr = curr->next;
+        }
+
+        return dummy.next;
+    }
+};
