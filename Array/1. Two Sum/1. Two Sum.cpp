adc8d832bfd259f5cfb32dd4b6d6a72df88930c6
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-08 19:36:11 +0530

    CodePusher: Solution for 1. Two Sum

--- Array/1. Two Sum/1. Two Sum.cpp
+++ Array/1. Two Sum/1. Two Sum.cpp
@@ -1,6 +1,6 @@
 // Problem: 1. Two Sum
 // Runtime: 0 ms (Beats 100.00%)
-// Memory: 14.9 MB (Beats 40.72%)
+// Memory: 15 MB (Beats 23.32%)
 
 class Solution {
 public:
@@ -10,10 +10,10 @@ public:
 
         for (int i = 0; i < nums.size(); i++) {
 
-            int complement = target - nums[i];
+            int need = target - nums[i];
 
-            if (mp.find(complement) != mp.end()) {
-                return {mp[complement], i};
+            if (mp.find(need) != mp.end()) {
+                return {mp[need], i};
             }
 
             mp[nums[i]] = i;
