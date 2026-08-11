e3b214609ceece8e969417ee92eca38cf6ede921
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-11 18:09:41 +0530

    CodePusher: Solution for 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum

---
+++ Array/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum/2996. Smallest Missing Integer Greater Than Sequential Prefix Sum.cpp
@@ -0,0 +1,27 @@
+// Problem: 2996. Smallest Missing Integer Greater Than Sequential Prefix Sum
+// Runtime: 7 ms (Beats 5.76%)
+// Memory: 23 MB (Beats 29.32%)
+
+class Solution {
+public:
+    int missingInteger(vector<int>& nums) {
+        int sum = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] + 1) {
+                sum += nums[i];
+            }else
+            {
+                break;
+            }
+        }
+
+        unordered_set<int> seen(nums.begin(), nums.end());
+
+        while (seen.count(sum)) {
+            sum++;
+        }
+
+        return sum;
+    }
+};
