466c7e70fb61f89be830cc5652919e11e8c344af
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-17 22:40:19 +0530

    CodePusher: Solution for 50. Pow(x, n)

---
+++ Math/50. Powx n/50. Powx n.cpp
@@ -0,0 +1,10 @@
+// Problem: 50. Pow(x, n)
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 8.9 MB (Beats 7.58%)
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        return pow(x,n);
+    }
+};
