6a9785b73fbac9ac11600e323ff423633265264a
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-17 21:23:34 +0530

    CodePusher: Solution for 452. Minimum Number of Arrows to Burst Balloons

---
+++ Array/452. Minimum Number of Arrows to Burst Balloons/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -0,0 +1,28 @@
+// Problem: 452. Minimum Number of Arrows to Burst Balloons
+// Runtime: 40 ms (Beats 87.70%)
+// Memory: 94 MB (Beats 56.85%)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(),
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+
+        int arrows = 1;
+        long long arrowPosition = points[0][1];
+
+        for (int i = 1; i < points.size(); i++) {
+            if (points[i][0] > arrowPosition) {
+                arrows++;
+                arrowPosition = points[i][1];
+            }
+        }
+
+        return arrows;
+    }
+};
