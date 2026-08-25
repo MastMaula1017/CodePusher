c3ecf00187741c01715a3c78c31bc888ffacf6f6
Vansh Raturi <vansh.12320821@lpu.in>
2026-08-25 23:35:14 +0530

    CodePusher: Solution for 3718. Smallest Missing Multiple of K

--- Array/3718. Smallest Missing Multiple of K/3718. Smallest Missing Multiple of K.cpp
+++ Array/3718. Smallest Missing Multiple of K/3718. Smallest Missing Multiple of K.cpp
@@ -1,15 +1,18 @@
 // Problem: 3718. Smallest Missing Multiple of K
-// Runtime: 7 ms (Beats 9.55%)
-// Memory: 25.1 MB (Beats 45.06%)
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 24 MB (Beats 94.59%)
 
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        unordered_set<int> st(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
         int multiple = k;
-        while (st.count(multiple)) {
-            multiple += k;
+
+        for (int x : nums) {
+            if (x == multiple) {
+                multiple += k;
+            }
         }
 
         return multiple;
