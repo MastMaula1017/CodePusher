21ff4fd698c85be081583f584b14f0e55c4ca8a1
Vansh Raturi <vansh.12320821@lpu.in>
2026-09-02 20:32:59 +0530

    CodePusher: Solution for 3875. Construct Uniform Parity Array I

---
+++ Array/3875. Construct Uniform Parity Array I/3875. Construct Uniform Parity Array I.cpp
@@ -0,0 +1,10 @@
+// Problem: 3875. Construct Uniform Parity Array I
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 30.3 MB (Beats 36.43%)
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+    }
+};
