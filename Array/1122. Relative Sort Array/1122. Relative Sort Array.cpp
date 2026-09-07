50ebb41db5af543ab56e1e3432ef30e7fd071b30
Vansh Raturi <vansh.12320821@lpu.in>
2026-09-07 19:40:15 +0530

    CodePusher: Solution for 1122. Relative Sort Array

---
+++ Array/1122. Relative Sort Array/1122. Relative Sort Array.cpp
@@ -0,0 +1,33 @@
+// Problem: 1122. Relative Sort Array
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 11.2 MB (Beats 19.66%)
+
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        map<int, int> mp;
+
+        for (int &num : arr1) {
+            mp[num]++;
+        }
+
+        int i = 0;
+
+        for (int &num : arr2) {
+            while (mp[num]-- > 0) {
+                arr1[i++] = num;
+            }
+        }
+
+        for (auto &it : mp) {
+            int freq = it.second;
+
+            while (freq > 0) {
+                arr1[i++] = it.first;
+                freq--;
+            }
+        }
+
+        return arr1;
+    }
+};
