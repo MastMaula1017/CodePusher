04acc6a4843b496dcbe844d566fd32630355e024
Vansh Raturi <vansh.12320821@lpu.in>
2026-09-20 19:03:47 +0530

    CodePusher: Solution for 3498. Reverse Degree of a String

---
+++ String/3498. Reverse Degree of a String/3498. Reverse Degree of a String.cpp
@@ -0,0 +1,18 @@
+// Problem: 3498. Reverse Degree of a String
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 9.8 MB (Beats 15.78%)
+
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = 26 - (s[i] - 'a');
+
+            ans += value * (i + 1);
+        }
+
+        return ans;
+    }
+};
