0deb6fb07b8b9eca7bf53f49c57972c7b23352c5
Vansh Raturi <vansh.12320821@lpu.in>
2026-09-21 17:48:25 +0530

    CodePusher: Solution for 2769. Find the Maximum Achievable Number

---
+++ Math/2769. Find the Maximum Achievable Number/2769. Find the Maximum Achievable Number.cpp
@@ -0,0 +1,10 @@
+// Problem: 2769. Find the Maximum Achievable Number
+// Runtime: 0 ms (Beats 100.00%)
+// Memory: 9.5 MB (Beats 78.11%)
+
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+         return num + 2 * t;
+    }
+};
